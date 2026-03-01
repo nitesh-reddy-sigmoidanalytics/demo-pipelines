2cd6a06f76284acef07a52e6aca8d8671242fa4c
Sigmoid <sigmoid@your.domain.com>
2026-03-01 23:29:48 +0530

    added new dags

---
+++ ecommerce_etl.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pandas as pd
+from common.db import execute_query
+
+RAW_PATH = "/opt/airflow/data/orders.csv"
+
+def extract():
+    df = pd.read_csv(RAW_PATH)
+    return df.to_json()
+
+def transform(ti):
+    df = pd.read_json(ti.xcom_pull())
+
+    df = df[df["status"] == "DELIVERED"]
+    df["revenue"] = df["quantity"] * df["price"]
+
+    return df.to_json()
+
+def load(ti):
+    df = pd.read_json(ti.xcom_pull())
+
+    for _, row in df.iterrows():
+        execute_query("""
+            INSERT INTO orders_dw
+            (order_id, customer_id, product, quantity, price, revenue, order_date)
+            VALUES (%s,%s,%s,%s,%s,%s,)
+            ON CONFLICT (order_id) DO NOTHING
+        """, tuple(row))
+
+with DAG(
+    "ecommerce_orders_pg",
+    start_date=datetime(2026, 2, 1),
+    schedule_interval="@weekly",
+    catchup=False,
+) as dag:
+
+    PythonOperator(task_id="extract", python_callable=extract) >> \
+    PythonOperator(task_id="transform", python_callable=transform) >> \
+    PythonOperator(task_id="load", python_callable=load)
---
+++ web_logs_etl.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pandas as pd
+from utils.db import execute_query
+
+RAW_PATH = "/opt/airflow/data/web_logs.json"
+
+def extract():
+    df = pd.read_json(RAW_PATH)
+    return df.to_json()
+
+def transform(ti):
+    df = pd.read_json(ti.xcom_pull())
+
+    metrics = {
+        "total_requests": len(df),
+        "unique_users": df["user_id"].nunique(),
+        "error_rate": (df["status"] >= 400).mean()
+    }
+
+    return metrics
+
+def load(ti):
+    m = ti.xcom_pull()
+
+    execute_query("""
+        INSERT INTO web_metrics
+        VALUES (NOW, %s, %s, %s)
+    """, (m["total_requests"], m["unique_users"], m["error_rate"]))
+
+with DAG(
+    "web_logs_pg",
+    start_date=datetime(2026, 2, 1),
+    schedule_interval="@weekly",
+    catchup=False,
+) as dag:
+
+    PythonOperator(task_id="extract", python_callable=extract) >> \
+    PythonOperator(task_id="transform", python_callable=transform) >> \
+    PythonOperator(task_id="load", python_callable=load)
